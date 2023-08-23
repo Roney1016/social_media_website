9e9b9e71087bef72d7309988a9a3aedc96aed7d7
manish kumar ravi <manishkumarravi1016@gmail.com>
2023-08-23 18:36:09 +0530

    creact express app

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');// require cookies parser (req.cookies)
+const db = require('./config/mongoose')
+const port = process.env.PORT || 3000;
+const app = express();
+
+
+app.get('/',function(req,res){
+    // return res.render('home',{heading:'hello sever'});
+    return res.send('hello buddy !')
+})
+
+
+
+app.listen(port, function (err) {
+    if (err) {
+        console.log(`error in running ther sever ${err}`)
+    }
+    console.log(`sever is running on the port : ${port}`)
+})
