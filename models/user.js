75d448d6474ab4866f24dcc3ac048a793b14cc15
manish kumar ravi <manishkumarravi1016@gmail.com>
2023-08-23 19:34:55 +0530

    commit

--- models/user.js
+++ models/user.js
@@ -9,7 +9,7 @@ const userSchema = new mongoose.Schema({
     password: {
         type: String,
         required: true,
-        unique: true
+        // unique: true
     },
     name: {
         type: String,
