434c06e26774f10b804c3863259658993d6243ee
manish kumar ravi <manishkumarravi1016@gmail.com>
2023-08-23 19:10:05 +0530

    create controller for create new user

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -6,3 +6,46 @@ module.exports.signUp = function (req, res) {
 module.exports.signIn = function (req, res) {
     return res.render('user_sign_in', { title: 'sign in' });
 }
+
+module.exports.create = async function(req,res){
+    if(req.body.password != req.body.confirm_password){
+        return res.redirect('back')
+    }
+
+    // User.find({email:req.body.email},function(err,user){ //
+    //     if(err){
+    //         console.log('error in finding user while signing up')
+    //         return;
+    //     }
+    //     if(!user){ // if new user then create user
+    //         User.create(req.body,function(err,user){
+    //             if(err){
+    //                 console.log('error in creating user while signing up ');return;
+    //             }
+
+    //             return res.redirect('user/sign_in')
+    //         })
+    //     }else{  // if user is already exit 
+    //         return res.redirect('back')
+    //     }
+    // })
+
+    try {
+        const existingUser = await User.findOne({ email: req.body.email });
+
+        if (!existingUser) {
+            const newUser = await User.create(req.body);
+            return res.redirect('/user/sign_in');
+        } else {
+           
+            return res.redirect('back');
+        }
+    } catch (err) {
+        console.error('Error:', err);
+        return res.status(500).send('Internal Server Error');
+    }
+}
+// sign in and create a session for the user
+module.exports.createSession = function(req,res){
+
+}
--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,9 @@ const router =express.Router();
 
 router.get('/sign_up',controller.signUp)  //render sign up page
 
-router.get('/sign_in',controller.signIn);  // render sign in page
+router.get('/sign_in',controller.signIn);  // render sign in page 
+
+router.post('/create',controller.create)   // create new user
 
 
 module.exports = router
