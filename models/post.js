d81934be732bb5f03ae2030fbfeac2e1cdb25332
manish kumar ravi <manishkumarravi1016@gmail.com>
2023-08-27 00:18:11 +0530

    add comment schema and form comment and add comments id array in post schema

--- models/post.js
+++ models/post.js
@@ -1,18 +1,25 @@
 //database relations
 
-const mongoose =require('mongoose');
+const mongoose = require('mongoose');
 
 const postSchema = new mongoose.Schema({
-    content:{
+    content: {
         type: String,
         required: true
     },
     user: {
         type: mongoose.Schema.ObjectId,
-        ref:'User'
-    }
-},{timestamps:true});
+        ref: 'User'
+    },
+    //include the array of ids of all comments in this post schema itself
+    comments: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Comment'
+        }
+    ]
+}, { timestamps: true });
 
-const Post = mongoose.model('Post',postSchema);
+const Post = mongoose.model('Post', postSchema);
 
 module.exports = Post;
