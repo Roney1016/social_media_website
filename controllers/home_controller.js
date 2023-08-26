08dac5a1facc1ba2d5a888f214185e3c7d65d243
manish kumar ravi <manishkumarravi1016@gmail.com>
2023-08-26 23:04:36 +0530

    desplay posts and  related user

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,6 +1,20 @@
+const Post = require('../models/post');
+
+
 module.exports.home = function (req, res) {
     // console.log(req.cookies);
     // res.cookie('user_id',25);
 
-    return res.render('home', { title: 'home' })
+
+    //  Post.find({})
+    //  .then(posts=>{
+    //     return res.render('home', { title: 'home' , posts:posts})
+    //  })
+
+    //   populate the user of each post
+    Post.find({}).populate('user').exec()
+    .then(posts=>{
+        return res.render('home', { title: 'home' , posts:posts})
+    })
+    
 }
